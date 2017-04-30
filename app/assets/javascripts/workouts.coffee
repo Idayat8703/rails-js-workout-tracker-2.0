991f7d863abae9b20130d3db6eb182a72185eace
Idayat <github email address>
2017-04-30 19:13:29 -0400

    add comments serializer

--- app/assets/javascripts/workouts.coffee
+++
@@ -1,8 +0,0 @@
-function Comment (user_email, body) {
-  this.user_email = user_email;
-  this.body = body;
-}
-
-Comment.prototype.addComment = function() {
-  $('.recipeComments').append('<p><strong>' + this.user_email + ' says:</strong><br>' + this.body + '</p>')
-}
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+
+  def index
+    @comments = Meal.find(params[:meal_id]).comments
+    render json: @comments
+  end
+
+  def create
+    recipe = Meal.find(params[:meal_id])
+    @comment = meal.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+    render json: @comment, status: 201
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :meal_id, :user_id)
+  end
+
+end
